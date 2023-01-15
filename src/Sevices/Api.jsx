1a92c378fbe2aa82335e165bef75933a569d8cb1
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2023-01-15 12:57:35 +0100

    add Movies/Home and some style

--- src/Sevices/Api.jsx
+++ src/Sevices/Api.jsx
@@ -1,4 +1,52 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 // const KEY = '81d5139447ae9c7175710aacae6d5810';
 // axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const trandingMovie = 'trending/movie/day';
+const nameMovie = 'search/movie';
+
+const movieApi = axios.create({ baseURL: 'https://api.themoviedb.org/3/' });
+export const moviesFetch = async () => {
+  const response = await movieApi.get(trandingMovie, {
+    params: { api_key: '81d5139447ae9c7175710aacae6d5810' },
+  });
+  return response.data.results;
+};
+
+export const searchMoviesByName = async searchQuery => {
+  const response = await movieApi.get(`/${nameMovie}`, {
+    params: {
+      api_key: '81d5139447ae9c7175710aacae6d5810',
+      query: searchQuery,
+    },
+  });
+  return response.data.results;
+};
+
+export const searchMovieById = async id => {
+  const response = await movieApi.get(`/movie/${id}`, {
+    params: {
+      api_key: '81d5139447ae9c7175710aacae6d5810',
+    },
+  });
+  return response.data;
+};
+
+export const searchMovieByCast = async id => {
+  const response = await movieApi.get(`/movie/${id}/credits`, {
+    params: {
+      api_key: '81d5139447ae9c7175710aacae6d5810',
+    },
+  });
+  return response.data;
+};
+
+export const searchMovieByReviews = async id => {
+  const response = await movieApi.get(`/movie/${id}/reviews`, {
+    params: {
+      api_key: '81d5139447ae9c7175710aacae6d5810',
+    },
+  });
+  return response.data;
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from './Layout/Layout';
+import { Suspense, lazy } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import css from './App.module.css';
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('./Movies/MoviesDetails'));
+// const Cast = lazy(() => import('components/Cast/Cast'));
+// const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}></Route>
-    </Routes>
+    <>
+      <nav className={css.nav}>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="movies">Movies</NavLink>
+      </nav>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="movies" element={<Movies />}></Route>
+          <Route path="movies/:moviesId" element={<MovieDetails />}>
+            {/* <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route> */}
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 };
