409b86abaa73a69df249001d59c2d5b3fd9001e5
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2023-01-15 11:35:17 +0100

    add Nav/layout

---
+++ src/Sevices/Api.jsx
@@ -0,0 +1,4 @@
+// import axios from 'axios';
+
+// const KEY = '81d5139447ae9c7175710aacae6d5810';
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Navigation } from 'components/Navigation/Navigation';
+// import { Outlet, useLocation } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main></main>
+      {/* <footer>Footer</footer> */}
+    </>
+  );
+};
